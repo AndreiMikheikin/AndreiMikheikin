a40b679412dca5eca4e4c8c8970a233ce5371162
AndreiMikheikin <149661314+AndreiMikheikin@users.noreply.github.com>
2024-09-12 19:14:17 +0300

    -----

--- MenuHelperApp/firebase-config.js
+++ MenuHelperApp/firebase-config.js
@@ -1,10 +1,3 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAjJIU3lCSJivMJ9UbihXD1dqu8ivf-8OU",
-  authDomain: "menuhelperapp.firebaseapp.com",
-  databaseURL: "https://menuhelperapp-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "menuhelperapp",
-  storageBucket: "menuhelperapp.appspot.com",
-  messagingSenderId: "118002716868",
-  appId: "1:118002716868:web:2623db6910ab0771c87991",
-  measurementId: "G-RNTTG6L21G"
+ 
 };
