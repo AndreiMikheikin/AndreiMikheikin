512b7806c2ea2c2c395bebc02de0dc3b8260728b
AndreiMikheikin <149661314+AndreiMikheikin@users.noreply.github.com>
2024-09-12 09:10:18 +0300

    debug

--- MenuHelperApp/firebase-config.js
+++ MenuHelperApp/firebase-config.js
@@ -1,25 +1,8 @@
-// src/firebase-config.js
-
-// Импорт необходимых функций из Firebase SDK
-import { getApp, getApps, initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, signOut, fetchSignInMethodsForEmail } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
-
-// Конфигурация Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyAjJIU3lCSJivMJ9UbihXD1dqu8ivf-8OU",
-    authDomain: "menuhelperapp.firebaseapp.com",
-    projectId: "menuhelperapp",
-    storageBucket: "menuhelperapp.appspot.com",
-    messagingSenderId: "118002716868",
-    appId: "1:118002716868:web:2623db6910ab0771c87991"
-};
-
-// Инициализация Firebase приложения
-// Если приложение уже инициализировано, используем его, иначе инициализируем новое
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-
-// Инициализация сервиса аутентификации Firebase
-const auth = getAuth(app);
-
-// Экспорт auth для использования в других частях приложения
-export { auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, signOut, fetchSignInMethodsForEmail };
+export const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+  };
